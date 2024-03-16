fa4778668565b47da281674d1ab93f34ef95a219
Aleh Makaranka <29537473+cpoftea@users.noreply.github.com>
2024-03-17 02:21:53 +0400

    Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,11 +5,31 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'Harmonia Night v1'
-config.bold_brightens_ansi_colors = false
-config.font_size = 18
-config.font = wezterm.font 'BlexMono Nerd Font Mono'
+config.prefer_to_spawn_tabs = true
+config.enable_scroll_bar = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_update_window = true
 
+-- Font
+config.font_size = 14
+config.font = wezterm.font 'BlexMono Nerd Font Mono'
+config.adjust_window_size_when_changing_font_size = false
+
+-- Macos specific
+config.macos_window_background_blur = 20
+config.native_macos_fullscreen_mode = true
+
+-- Style
+config.window_background_opacity = 0.96
+config.bold_brightens_ansi_colors = false
+config.color_scheme = 'Harmonia Night v1'
 config.color_schemes = {
 	['Harmonia Night v1'] = {
 		background = '#222020',
