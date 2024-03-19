9ca463209296e8b6c87e8431597095dfccfcdc87
Aleh Makaranka <29537473+cpoftea@users.noreply.github.com>
2024-03-20 01:34:16 +0400

    Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.prefer_to_spawn_tabs = true
 config.enable_scroll_bar = true
 config.window_padding = {
 	left = 0,
@@ -15,6 +14,7 @@ config.window_padding = {
 }
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 config.show_update_window = true
 
 -- Font
